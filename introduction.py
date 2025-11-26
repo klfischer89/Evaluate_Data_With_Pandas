d3f57a7e252ea910103f1f63f05b0bc4c4a27cc4
klfischer89 <fischer-k4@web.de>
2025-11-26 14:06:40 +0100

    read csv file with semicolon seperator

--- introduction.py
+++ introduction.py
@@ -1,6 +1,9 @@
 import pandas as pd # import pandas as pd
 
-df = pd.read_csv("teilnehmer.csv") # read csv file and safe as dataframe
-print(df.head())
-print(df) # output dataframe
+# df = pd.read_csv("teilnehmer.csv") # read csv file and safe as dataframe
+# print(df.head())
+# print(df) # output dataframe
 
+df = pd.read_csv("teilnehmer-semikolon.csv", sep = ";") # read file with semicolon delimeter, sep can be \t as well for tab
+# use "" to escape seperators in csv file
+print(df)
