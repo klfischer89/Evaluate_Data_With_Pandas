3c67f6b57edfb8f00bfb99dd9f004db2b6d07960
klfischer89 <fischer-k4@web.de>
2025-11-27 14:40:35 +0100

    sort data

--- datatypes_Pandas.py
+++ datatypes_Pandas.py
@@ -17,10 +17,28 @@ df = df.dropna() # get only data that ist not NaN
 df["City"] = df["City"] + ", " + df["Country"]
 df.drop(["Country"], axis=1, inplace=True)
 
-print(df.head())
+# print(df.head())
 
 df_city = df["City"].str.split(",", n=1, expand=True)
 df["City"] = df_city[0]
 df["Country"] = df_city[1]
 
-print(df.head())
+# print(df.head())
+
+longitudeDir = df["Longitude"].str[-1]
+df["Longitude"] = df["Longitude"].str[:-1].astype("float")
+df.loc[longitudeDir == "W", "Longitude"] = df["Longitude"] * -1
+
+latDir = df["Latitude"].str[-1]
+df["Latitude"] = df["Latitude"].str[:-1].astype("float")
+df.loc[latDir == "S", "Latitude"] = df["Latitude"] * -1
+
+print(df.head)
+
+df["dt"] = pd.to_datetime(df["dt"])
+
+# print(df.head)
+print(df.loc[df["dt"].dt.year == 1849, "AverageTemperature"].mean())
+print(df.loc[df["dt"].dt.year == 2012, "AverageTemperature"].mean())
+
+df.sort_values(by=["AverageTemperature"], inplace=True, ascending=False)
